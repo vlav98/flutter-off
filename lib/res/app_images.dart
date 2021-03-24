161d6a882bd7c64032fc97978420df40c51bb569
Valentine <valentinelav98@gmail.com>
2021-03-24 16:47:34 +0100

    Move and correct structure ffs2.0

--- lib/res/app_images.dart
+++ lib/res/app_images.dart
@@ -1,10 +1,10 @@
 class AppImages {
   AppImages._();
 
-  static const String nutriscoreA = 'lib/res/drawables/nutriscore_a.png';
-  static const String nutriscoreB = 'lib/res/drawables/nutriscore_b.png';
-  static const String nutriscoreC = 'lib/res/drawables/nutriscore_c.png';
-  static const String nutriscoreD = 'lib/res/drawables/nutriscore_d.png';
-  static const String nutriscoreE = 'lib/res/drawables/nutriscore_e.png';
-  static const String pancakes = 'lib/res/drawables/pancakes.jpg';
+  static const String nutriscoreA = 'res/drawables/nutriscore_a.png';
+  static const String nutriscoreB = 'res/drawables/nutriscore_b.png';
+  static const String nutriscoreC = 'res/drawables/nutriscore_c.png';
+  static const String nutriscoreD = 'res/drawables/nutriscore_d.png';
+  static const String nutriscoreE = 'res/drawables/nutriscore_e.png';
+  static const String pancakes = 'res/drawables/pancakes.jpg';
 }
